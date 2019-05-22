4dd5f251c2317cc20e24187ff0797af6f42c5185
Dymion01 <damianskorupa4@gmail.com>
2019-05-22 16:47:03 +0200

    adding dictionary of courses

--- searchingCourses/searchingCourses/Program.cs
+++ searchingCourses/searchingCourses/Program.cs
@@ -6,10 +6,20 @@ namespace searchingCourses
     {
         static void Main(string[] args)
         {
-            var songlyrics = new SongLyrics("Shakira", "Nada");
-
+          //var songlyrics = new SongLyrics("Shakira", "Nada");//
+           var profanityFinder = new ProfanityFinder("no , kurwa!"):
             Console.WriteLine("Done.");
             Console.ReadLine();
+        }
+    }
+
+    internal class ProfanityFinder
+    {
+        
+
+        public ProfanityFinder(string text 
+        {
+            var dictFile = FileStyleUriParser.ReadAllText( "../profan
         }
     }
 }
